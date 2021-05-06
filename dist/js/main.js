f2a9df7f81dfa176a27ce2ca2815e63840826c6e
happilyafter <happilyafter.invitation@gmail.com>
2021-05-06 15:29:04 +0700

    second commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-(function($) {
+!(function($) {
     "use strict";
     
     // Set the count down timer
@@ -10,6 +10,38 @@
         });
     }
     
-   
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+        } else {
+        $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+        scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    //get url custom
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+        }
+        
+        var sn = getParameterByName('to');
+        document.getElementById("write").innerHTML = sn;
+
+        
+    // Modal Show
+    $('#myModal').modal('show');
     
 })(jQuery);
